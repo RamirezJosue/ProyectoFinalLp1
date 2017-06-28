eaaef4992866dd2e5a8543ef1f78a5274be7b95f
RamirezJosue <xdemobtkx@gmail.com>
2017-06-28 15:19:09 -0500

    caso de uso ventas

--- frontendupeu/js/controllers/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-app.controller('mainController', ['$scope', '$rootScope', '$http', function ($scope, $rootScope, $http) {
-        console.log('Sistema de Mensajería');
-        $(function () {
-            $("[data-tooltip='tooltip']").tooltip();
-        });
-    }]);
